b6644bb1d31c2d6e88c915a2c347ad9170610eef
Oleksandr Petrovskyi <sashalenz@gmail.com>
2021-03-28 13:02:54 +0300

    polish

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,9 +4,5 @@ namespace Sashalenz\Binotel\Tests;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
-    public function true_is_true(): void
-    {
-        self::assertTrue(true);
-    }
+
 }
